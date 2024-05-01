f3925af3575e70bfc92f6fef822efed2e47473ae
Elisa Andres <elisaandresmolina22@gmail.com>
2024-04-30 23:42:54 -0600

    first commit

---
+++ src/main/java/com/siiau/udg/controllers/StudentController.java
@@ -0,0 +1,55 @@
+package com.siiau.udg.controllers;
+
+import com.siiau.udg.models.StudentModel;
+import com.siiau.udg.services.StudentServices;
+import com.siiau.udg.services.StudentServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    @Autowired
+    StudentServices service;
+
+    @PostMapping()
+    public StudentModel saveStudent(@RequestBody StudentModel student){
+        return service.saveStudent(student);
+    }
+
+    @PutMapping()
+    public StudentModel editStudent(@RequestBody StudentModel student){
+        return service.editStudent(student);
+    }
+
+    @GetMapping()
+    public ArrayList<StudentModel> getAllStudents(){
+        return service.getAllStudents();
+    }
+
+
+    @GetMapping("/findStudentById/{id}")
+    public Optional<StudentModel> findStudentById(@PathVariable Long id){
+        return service.findStudentById(id);
+    }
+
+    @GetMapping("/findStudentByCode/{code}")
+    public Optional<ArrayList<StudentModel>> finStudentByCode(@PathVariable String code){
+        return service.findStudentByCode(code);
+    }
+
+
+    @GetMapping("/findStudentByName/{name}")
+    public Optional<ArrayList<StudentModel>> finStudentByName(@PathVariable String name){
+        return service.findStudentByName(name);
+    }
+    @DeleteMapping("/delete-student-by-id")
+    public String deleteStudentById(@RequestParam("id") Long id){
+        return service.deleteStudentById(id);
+    }
+
+
+}
---
+++ src/test/java/com/siiau/udg/UdgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siiau.udg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
